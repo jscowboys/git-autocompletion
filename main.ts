b04f94552b44bca1a24fece82457f84ef918d900
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-08-28 16:12:39 -0300

    Add shell config step I

--- main.ts
+++ main.ts
@@ -4,7 +4,6 @@ import { configureShell } from './shell.ts';
 console.log(`🧑🏽‍💻 starting...`);
 await scriptsDownload();
 console.log(`🧑🏽‍💻 configuring shell..`);
-// await configureShell();
-console.log(`✅ shell configured!`);
+await configureShell();
 console.log(`finished! 🙌 🙌`);
 Deno.exit();
--- shell.ts
+++ shell.ts
@@ -1,19 +1,32 @@
-import { homedir } from "./deps.ts";
-import { exec } from "https://deno.land/x/exec@0.0.5/mod.ts";
+import { homedir } from './deps.ts';
+import { OutputMode, exec } from 'https://deno.land/x/exec@0.0.5/mod.ts';
+
+const lines = `# Load Git completion \nzstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bash \nfpath=(~/.zsh $fpath) \n \nautoload -Uz compinit && compinit`;
+//const reloadShell = 'source ~/.zshrc';
 
 export async function configureShell() {
-  const lines =
-    `zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bash \n fpath=(~/.zsh $fpath) \n \n autoload -Uz compinit && compinit`;
-  const clearCache = "rm ~/.zcompdump";
-  const reloadShell = "source ~/.zshrc";
-  const dir = `${homedir()}/.zshrc`;
-
   try {
-    await Deno.writeTextFile(dir, `\n\n${lines}`, { append: true });
+    await Deno.writeTextFile(`${homedir()}/.zshrc`, `\n\n${lines}`, {
+      append: true,
+    });
     console.log(`✅ File saved!!`);
-    await exec(`${clearCache} && ${reloadShell}`);
-    console.log(`✅ Cache & Shell reloaded!!`);
+
+    try {
+      await Deno.remove(`${homedir()}/.zcompdump`); //Clear out the shell’s autocompletion cache
+    } catch (err) {
+      if (!(err instanceof Deno.errors.NotFound)) {
+        throw err;
+      }
+    }
+
+    const command = new Deno.Command(Deno.execPath(), {
+      args: ['source', `${homedir()}/.zshrc`],
+    });
+    const { code, stdout, stderr } = command.outputSync();
+    console.log('HOLAAAA', code.toString(), stdout.toString(), stderr);
+
+    console.log(`✅ Shell configured!`);
   } catch (error) {
-    return error.message;
+    console.error(error);
   }
 }
