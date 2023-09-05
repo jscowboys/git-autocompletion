52b41d465fc6491f11def7264751726f30491735
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-09-04 22:25:15 -0300

    Change import for using imports defined in deno.json

--- src/deps.ts
+++ src/deps.ts
@@ -3,6 +3,6 @@
  *
  * This module re-exports the required methods from the dependant remote Ramda module.
  */
-export { homedir } from 'https://deno.land/std@0.110.0/node/os.ts';
-export { exists } from 'https://deno.land/std@0.197.0/fs/mod.ts';
+export { homedir } from 'std/node/os.ts';
+export { exists } from 'std/fs/mod.ts';
 export { download } from 'https://deno.land/x/download@v2.0.2/mod.ts';
