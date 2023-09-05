796ae5f4de26ca21e76cfb02778c3ca76227b0fb
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-09-05 15:56:17 -0300

    Add download test & test task

---
+++ test/download.test.ts
@@ -0,0 +1,27 @@
+import { assertEquals, assertArrayIncludes } from 'std/assert/mod.ts';
+import { download, homedir } from '../src/deps.ts';
+import { gitBashUrl, gitCompletionUrl } from '../src/constants.ts';
+
+const dir = `${homedir()}/test`;
+
+Deno.test('test download command', async () => {
+  try {
+    await download(gitBashUrl, { file: '.testGit', dir });
+    await download(gitCompletionUrl, { file: '.testGitCompletion', dir });
+
+    const fileNames: string[] = [];
+
+    for await (const dirEntry of Deno.readDir(dir)) {
+      if (dirEntry.isFile) {
+        fileNames.push(dirEntry.name);
+      }
+    }
+
+    assertEquals(fileNames.length, 2);
+    assertArrayIncludes(fileNames, ['.testGit', '.testGitCompletion']);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await Deno.remove(dir, { recursive: true });
+  }
+});
