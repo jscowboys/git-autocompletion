c1af22d27a898849eb5a2f8f8c7ad42d0fdd1d64
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-09-04 22:53:52 -0300

    Rollback homedir import

--- src/deps.ts
+++ src/deps.ts
@@ -3,6 +3,6 @@
  *
  * This module re-exports the required methods from the dependant remote Ramda module.
  */
-export { homedir } from 'std/node/os.ts';
+export { homedir } from 'https://deno.land/std@0.110.0/node/os.ts';
 export { exists } from 'std/fs/mod.ts';
 export { download } from 'https://deno.land/x/download@v2.0.2/mod.ts';
