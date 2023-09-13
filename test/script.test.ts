7004ec6b22047383d0e7c1aa244cf85c37996851
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-09-13 19:22:36 -0300

    Rollback tests

--- test/script.test.ts
+++ test/script.test.ts
@@ -4,33 +4,33 @@ import { gitBashUrl, gitCompletionUrl } from '../src/constants.ts';
 
 const dir = `${homedir()}/GitAutocompletion-${new Date().getTime()}/test`;
 
-// Deno.test('Test valid github links & download', async () => {
-//   try {
-//     await download(gitBashUrl, { file: '.testGit', dir });
-//     await download(gitCompletionUrl, { file: '.testGitCompletion', dir });
+Deno.test('Test valid github links & download', async () => {
+  try {
+    await download(gitBashUrl, { file: '.testGit', dir });
+    await download(gitCompletionUrl, { file: '.testGitCompletion', dir });
 
-//     const fileNames: string[] = [];
+    const fileNames: string[] = [];
 
-//     for await (const dirEntry of Deno.readDir(dir)) {
-//       if (dirEntry.isFile) {
-//         fileNames.push(dirEntry.name);
-//       }
-//     }
+    for await (const dirEntry of Deno.readDir(dir)) {
+      if (dirEntry.isFile) {
+        fileNames.push(dirEntry.name);
+      }
+    }
 
-//     assertEquals(fileNames.length, 2);
-//     assertArrayIncludes(fileNames, ['.testGit', '.testGitCompletion']);
-//   } catch (err) {
-//     console.error(err);
-//   } finally {
-//     await Deno.remove(dir, { recursive: true });
-//   }
-// });
+    assertEquals(fileNames.length, 2);
+    assertArrayIncludes(fileNames, ['.testGit', '.testGitCompletion']);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await Deno.remove(dir, { recursive: true });
+  }
+});
 
-// Deno.test('Check exists', async () => {
-//   try {
-//     const dirExists = await exists(dir);
-//     assertEquals(dirExists, false);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// });
+Deno.test('Check exists', async () => {
+  try {
+    const dirExists = await exists(dir);
+    assertEquals(dirExists, false);
+  } catch (err) {
+    console.error(err);
+  }
+});
