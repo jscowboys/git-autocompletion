278d00a9c289a697923a9dad9cf9812a79297807
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-09-20 14:20:02 -0300

    Add fixes for naming tests

--- test/shell.test.ts
+++ test/shell.test.ts
@@ -1,4 +1,3 @@
-import { assertEquals } from 'std/assert/mod.ts';
 import { autocompleteIsPresent } from '../src/shell.ts';
 import { assertInstanceOf } from 'https://deno.land/std@0.202.0/assert/mod.ts';
 
---
+++ test/utils.test.ts
@@ -0,0 +1,36 @@
+import { assertEquals, assertArrayIncludes } from 'std/assert/mod.ts';
+import { download, exists, homedir } from '../src/deps.ts';
+import { gitBashUrl, gitCompletionUrl } from '../src/constants.ts';
+
+const dir = `${homedir()}/GitAutocompletion-${new Date().getTime()}`;
+
+Deno.test('Test valid github links & download', async () => {
+  try {
+    await download(gitBashUrl, { file: '.testGit', dir });
+    await download(gitCompletionUrl, { file: '.testGitCompletion', dir });
+
+    const fileNames: string[] = [];
+
+    for await (const dirEntry of Deno.readDir(dir)) {
+      if (dirEntry.isFile) {
+        fileNames.push(dirEntry.name);
+      }
+    }
+
+    assertEquals(fileNames.length, 2);
+    assertArrayIncludes(fileNames, ['.testGit', '.testGitCompletion']);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await Deno.remove(dir, { recursive: true });
+  }
+});
+
+Deno.test('Check exists', async () => {
+  try {
+    const dirExists = await exists(dir);
+    assertEquals(dirExists, false);
+  } catch (err) {
+    console.error(err);
+  }
+});
