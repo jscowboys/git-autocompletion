3aa7ee118b74ab62f674eddf01c09dbd867db25e
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-10-08 14:47:26 -0300

    Adding checks on clear cache when rejecting

--- test/shell.test.ts
+++ test/shell.test.ts
@@ -5,11 +5,18 @@ import {
 	assertEquals,
 } from 'https://deno.land/std@0.110.0/testing/asserts.ts';
 import {
+	returnsNext,
+	stub,
+} from 'https://deno.land/std@0.202.0/testing/mock.ts';
+import {
 	autocompleteIsPresent,
 	clearCache,
 	configureShell,
 } from '../src/shell.ts';
-import { assertInstanceOf } from 'https://deno.land/std@0.202.0/assert/mod.ts';
+import {
+	assertInstanceOf,
+	assertRejects,
+} from 'https://deno.land/std@0.202.0/assert/mod.ts';
 
 Deno.test('ConfigureShell exits if autocomplete is present', async () => {
 	try {
@@ -21,11 +28,18 @@ Deno.test('ConfigureShell exits if autocomplete is present', async () => {
 });
 
 Deno.test('Checks clearing cache', async () => {
+	stub(Deno, 'remove', async () => Promise.resolve());
+	await clearCache();
+});
+
+Deno.test('Checks clearing cache when no file is present', async () => {
 	try {
-		await clearCache();
+		stub(Deno, 'remove', async () => {
+			throw new Deno.errors.NotFound('No file present');
+		});
+		await assertRejects(() => clearCache(), Error);
 	} catch (error) {
 		assert(error);
-		assertEquals(error.name, 'AssertionError');
 	}
 });
 
