bf5abd769fb1e2a054899736d10187fd07011c3c
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-10-08 13:29:35 -0300

    Adding shell test

--- test/shell.test.ts
+++ test/shell.test.ts
@@ -1,9 +1,24 @@
 Deno.env.set('DENO_ENV', 'test');
 
-import { autocompleteIsPresent } from '../src/shell.ts';
+import {
+	assert,
+	assertEquals,
+	assertExists,
+} from 'https://deno.land/std@0.110.0/testing/asserts.ts';
+import { autocompleteIsPresent, configureShell } from '../src/shell.ts';
 import { assertInstanceOf } from 'https://deno.land/std@0.202.0/assert/mod.ts';
 
+Deno.test('ConfigureShell exits if autocomplete is present', async () => {
+	try {
+		await configureShell();
+	} catch (error) {
+		console.log(error, error.name);
+		assert(error);
+		assertEquals(error.name, 'AssertionError');
+	}
+});
+
 Deno.test('Autocomplete does not throw errors', async () => {
-  const autocomplete = new Boolean(await autocompleteIsPresent());
-  assertInstanceOf(autocomplete, Boolean);
+	const autocomplete = new Boolean(await autocompleteIsPresent());
+	assertInstanceOf(autocomplete, Boolean);
 });
